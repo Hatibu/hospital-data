76efe8c0ca71a98eb851f4a8bf57472b4cbbedcf
Hatibu <44012303+Hatibu@users.noreply.github.com>
2021-02-08 10:14:08 +0300

    First commit

--- survey_form.php
+++ survey_form.php
@@ -16,137 +16,56 @@
 </head>
 
 <body>
-    <div class="row">
-      <div class="col-md-3"><!--Empty space--></div>
-      <div class="col-md-6">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="survey-form">
-                 <!-- Qn 1-->
-           <h5>Je, huduma za vifaa vya kujifunglia kwa mama ipoje?</h5>
-           <!-- radio button 1-->
-          <div class="form-check">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-          <label class="form-check-label" for="flexRadioDefault1">
-           Nzuri sana
-          </label>
-        </div>
-         <!-- radio button 2-->
-          <div class="form-check">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-          <label class="form-check-label" for="flexRadioDefault1">
-           Nzuri 
-          </label>
-        </div>
-         <!-- radio button 3-->
-          <div class="form-check">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-          <label class="form-check-label" for="flexRadioDefault1">
-           Wastani
-          </label>
-        </div>
-         <!-- radio button 4-->
-          <div class="form-check">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-          <label class="form-check-label" for="flexRadioDefault1">
-           Mbaya
-          </label>
-        </div>
-         <!-- radio button 5-->
-          <div class="form-check">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-          <label class="form-check-label" for="flexRadioDefault1">
-           Mbaya sana
-          </label>
-        </div>
-              
-            </div>
-          </div>
-        </div>
-          <div class="survey-form">
-         
+ <section class="light">
 
-        <!-- Qn 2-->
-           <h5>Je, huduma ya dawa kwa mama mjamzito ipoje?</h5>
-           <!-- radio button 1-->
-          <div class="form-check">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-          <label class="form-check-label" for="flexRadioDefault1">
-           Nzuri sana
-          </label>
-        </div>
-         <!-- radio button 2-->
-          <div class="form-check">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-          <label class="form-check-label" for="flexRadioDefault1">
-           Nzuri 
-          </label>
-        </div>
-         <!-- radio button 3-->
-          <div class="form-check">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-          <label class="form-check-label" for="flexRadioDefault1">
-           Wastani
-          </label>
-        </div>
-         <!-- radio button 4-->
-          <div class="form-check">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-          <label class="form-check-label" for="flexRadioDefault1">
-           Mbaya
-          </label>
-        </div>
-         <!-- radio button 5-->
-          <div class="form-check">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-          <label class="form-check-label" for="flexRadioDefault1">
-           Mbaya sana
-          </label>
-        </div>
+  <h1>(Light) Radio</h1>
 
-        <!-- Qn 3-->
-           <h5>Je, miundombinu kwa ajili ya mama mjamzito ipoje?</h5>
-           <!-- radio button 1-->
-          <div class="form-check">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-          <label class="form-check-label" for="flexRadioDefault1">
-           Nzuri sana
-          </label>
-        </div>
-         <!-- radio button 2-->
-          <div class="form-check">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-          <label class="form-check-label" for="flexRadioDefault1">
-           Nzuri 
-          </label>
-        </div>
-         <!-- radio button 3-->
-          <div class="form-check">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-          <label class="form-check-label" for="flexRadioDefault1">
-           Wastani
-          </label>
-        </div>
-         <!-- radio button 4-->
-          <div class="form-check">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-          <label class="form-check-label" for="flexRadioDefault1">
-           Mbaya
-          </label>
-        </div>
-         <!-- radio button 5-->
-          <div class="form-check">
-          <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-          <label class="form-check-label" for="flexRadioDefault1">
-           Mbaya sana
-          </label>
-        </div>
-        <a href="#" class="btn btn-dashboard">Toa sasa</a>
-          </div><!-- End of col-6-->
-      <div class="col-md-3"><!--Empty space--></div>
-    </div>
+  <label>
+    <input type="radio" name="light" checked>
+    <span class="design"></span>
+    <span class="text">Option 1</span>
+  </label>
 
-  
+  <label>
+    <input type="radio" name="light">
+    <span class="design"></span>
+    <span class="text">Option 2</span>
+  </label>
+
+  <label>
+    <input type="radio" name="light">
+    <span class="design"></span>
+    <span class="text">Option 3</span>
+  </label>
+
+</section>
+
+<section class="light">
+
+  <h1>(Light) Radio</h1>
+
+  <label>
+    <input type="radio" name="light" checked>
+    <span class="design"></span>
+    <span class="text">Option 1</span>
+  </label>
+
+  <label>
+    <input type="radio" name="light">
+    <span class="design"></span>
+    <span class="text">Option 2</span>
+  </label>
+
+  <label>
+    <input type="radio" name="light">
+    <span class="design"></span>
+    <span class="text">Option 3</span>
+  </label>
+
+</section>
+
+
+
   <!-- Bootstrap core JavaScript -->
   <script src="vendor/jquery/jquery.min.js"></script>
   <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
